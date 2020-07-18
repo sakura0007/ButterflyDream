2737a5dc71ab759e6542616c0805500c13befbba
sakura007 <35498776+sakura0007@users.noreply.github.com>
2020-07-18 13:31:00 +0800

    上传

---
+++ assets/resources/Prefab/InteractiveKey/KEYUI.js
@@ -0,0 +1,23 @@
+﻿cc.Class({
+    extends: cc.Component,
+
+    properties: {
+    },
+
+    onLoad() {
+
+    },
+
+    start () {
+
+    },
+
+    Open_Win(event, customEventData) {
+        //前置衔接Win脚本
+        let Win = cc.find('Canvas/Win');
+        if (Win) {
+            cc.find('Canvas/Win').getComponent('Win').re_start(null, customEventData);
+        }
+    }
+    // update (dt) {},
+});
